db055b65f9aa23542fef11c13358662e0e9667e9
gordonastt <franciscoosmarflores@gmail.com>
2024-06-28 09:02:09 -0300

    ADD post|put|get en product.js

--- routes/products.js
+++ routes/products.js
@@ -1,146 +1,130 @@
 //Archivo q maneja las rutas de los productos
-const express = require('express')
-const router = express.Router()
-const db_ = require('../db/db')
-const app = express();
-
-app.use(express.json())
-
-let products = [
-  {
-    id: 1,
-    title: "Monitor SONY 24'",
-    precio: 120000
-  },
-  {
-    id: 2,
-    title: "Memoria RAM DDR3 Soddimm 8GB Hiksemi ",
-    precio: 15000
-  },
-  {
-    id: 3,
-    title: "Stream Deck ElGato 6 botones LCD ",
-    precio: 60000
-  },
-  {
-    id: 4,
-    title: "Teclado Mecanico Redragon",
-    precio: 50000
-  },
-  {
-    id: 5,
-    title: "Impresora HP Laserjet M111",
-    precio: 140000
-  },
-  {
-    id: 6,
-    title: "Gamepad Redragon Harrow Pro Wireless",
-    precio: 50000
-  },
-  {
-    id: 7,
-    title: "Auriculares Logitech H390 con micrófono",
-    precio: 40000
-  },
-  {
-    id: 8,
-    title: "Placa de Red PCI Express Wifi TP-Link",
-    precio: 20000
-  },
-  {
-    id: 9,
-    title: "Starlink Adaptador Ethernet Gen 2",
-    precio: 55000
-  },
-  {
-    id: 10,
-    title: "Fuente de alimentación Sentey Solid Series F550w'",
-    precio: 60000
-  },
-  {
-    id: 11,
-    title: "Fuente de alimentación 650w Sentey",
-    precio: 65000
-  },
-  {
-    id: 12,
-    title: "Memoria RAM Kingston Fury Beast DDR4 Gamer 16gb",
-    precio: 60000
-  },
-]
+const express = require("express");
+const router = express.Router();
+const db = require("../db/db");
 
 //1er endpoint
-router.get('/',(req, res)=>{
-  // res.json(products)
-  const consulta_sql = "SELECT * FROM productos";
-  db_.query(consulta_sql, (err, results)=>{
-    if(err){
-      return res.status(500).send(err); 
+//visualizar todos los productos.
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM productos";
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
     }
     res.send(results);
-  })
-})
+  });
+});
 
 //2do endpoint
-// el id q le envíe por parámetro lo busca dentro del array de productos
-router.get('/:id',(req, res)=>{
-  // 'product' busca en products el id q coincida con el parámetro q le enviamos.
-  // se parsea porq sino lo toma como un text
-  const product = products.find(prod => prod.id === parseInt(req.params.id)) 
-  // si el producto no se encuentra mostrará un error 404
-  if(!product) return res.status(404).send('Producto no encontrado');
-  // y si lo encuentra q lo muetre en pantalla
-  res.json(product)
-})
+// buscador de producto por nombre
+router.get("/:nombre_producto", (req, res) => {
+  const sql = "SELECT * FROM productos WHERE nombre_producto LIKE ?";
+  const { nombre_producto } = req.params;
+  db.query(sql, [`%${nombre_producto}%`], (err, result) => {
+    // los campos completos
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send(result);
+  });
+});
 
 //3er endpoint en MÉTODO POST
 //INSERT
-router.post('/',(req, res)=>{
-  const newProduct = {
-    id: products.length + 1, // agrega el producto
-    title: req.body.title,  // recibe el título
-    precio: req.body.precio // recibe el precio
-  }
-  products.push(newProduct) // agrego el objeto nuevo al array de productos
-  res.status(201).json(newProduct) // para q se muestre el producto nuevo en caso de q se agregue bien
-})
+router.post("/", (req, res) => {
+  const {
+    nombre_producto,
+    precio_producto,
+    descripcion_producto,
+    id_categoria,
+  } = req.body;
+  console.log(req.body);
+  const sql =
+    "INSERT INTO productos (nombre_producto, precio_producto, descripcion_producto, id_categoria) VALUES (?, ?, ?, ?)";
+  const values = [
+    nombre_producto,
+    precio_producto,
+    descripcion_producto,
+    id_categoria,
+  ];
+  db.query(sql, values, (err, result) => {
+    // los campos completos
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.status(201).send({
+      id_producto: result.insertId,
+      nombre_producto,
+      precio_producto,
+      descripcion_producto,
+      id_categoria,
+    });
+  });
+});
 
 //4to ENDPOINT
 //UPDATE
-router.put('/:id',(req,res)=>{
-  const product = products.find(prod => prod.id === parseInt(req.params.id))
-  if(!product) return res.status(404).send('Producto no encontrado');
-  // el atributo titulo se va a modificar de acuerdo a lo q envie o quedará como está
-  product.title = req.body.title || product.title 
-  product.precio = req.body.precio || product.precio 
-  // respuesta que se mostrará  
-  res.status(201).json(product)
-})
+router.put("/:id", (req, res) => {
+  console.log("asdasd");
+  const {
+    nombre_producto,
+    precio_producto,
+    descripcion_producto,
+    id_categoria,
+  } = req.body;
+  const { id } = req.params;
+  console.log(req.body);
+  console.log(req.params);
+  sql = `UPDATE productos SET                
+                nombre_producto = ?,
+                precio_producto = ?,
+                descripcion_producto = ?,
+                id_categoria = ?
+         WHERE id_producto = ?`;
+  const values = [
+    nombre_producto,
+    precio_producto,
+    descripcion_producto,
+    id_categoria,
+    id,
+  ];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      return res.status(500).send("error al seleccionar ", err);
+    }
+    if (result.affectedRows > 0)
+      res.status(201).send("Modificado correctamente");
+    else res.status(200).send("Producto sin encontrar");
+    //    nombre_producto,precio_producto,descripcion_producto,id_categoria,
+  });
+});
 
 //5to ENDPOINT
 //DELETE
-router.delete('/:id', (req, res) =>{
+router.delete("/:id", (req, res) => {
   // parsea el id q le ingresamos y lo busca en el listado
-  const productIndex = products.findIndex(prod => prod.id === parseInt(req.params.id)); 
-  if(productIndex == -1) return res.status(404).send('Producto no encontrado'); 
+  const productIndex = products.findIndex(
+    (prod) => prod.id === parseInt(req.params.id)
+  );
+  if (productIndex == -1) return res.status(404).send("Producto no encontrado");
 
-  const deleteProduct = products.splice(productIndex, 1); 
+  const deleteProduct = products.splice(productIndex, 1);
 
-  res.json(deleteProduct) // cuando finaliza muestra la película eliminada
-})
+  res.json(deleteProduct); // cuando finaliza muestra la película eliminada
+});
 
-
-//
+/**
 app.post("/items", (req, res) => {
   const { nombre, descripcion } = req.body;
   const sql = "INSERT INTO items (nombre, descripcion) VALUES (?,?)";
-  db_.query(sql_, [nombre, descripcion], (err, result) => { // los campos completos
+  db.query(sql_, [nombre, descripcion], (err, result) => { // los campos completos
     if (err) {
       return res.status(500).send(err);
     }
     res.status(201).send({ id: result.insertId, nombre, descripcion }); //todos los campos de la bd
   });
 });
-//
+*/
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 //config para levantar el servidor
-const express = require('express')
-const app = express()
-const PORT = 3000
-const productsRouter = require('./routes/products')
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const productsRouter = require("./routes/products");
 
-app.use(express.json()) // se recibirá un json
+app.use(express.json()); // se recibirá un json
 
-app.use('/products', productsRouter)  // en la dirección products es donde voy a enviar un array de objetos
-
+app.use("/products", productsRouter); // en la dirección products es donde voy a enviar un array de objetos
 
 app.listen(PORT, () => {
   console.log(`Corriendo en puerto ${PORT}`);
-})
+});
